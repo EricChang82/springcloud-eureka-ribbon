3016f43e0d80abba08b0e550621d3bdca0d033d7
EricChang82 <changlejc@Hotmail.com>
2019-04-17 15:26:04 +0800

    1

--- springcloud-eureka-ribbon/src/main/java/com/example/config/EurekaRibbonConfig.java
+++ springcloud-eureka-ribbon/src/main/java/com/example/config/EurekaRibbonConfig.java
@@ -5,11 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+
+/**
+ * @author changle 
+ * Create Time: 2019年4月17日 
+ * Purpose: 实例化RestTemplate对象到容器，供 controller使用
+ */
 @Configuration
 public class EurekaRibbonConfig {
  
-    @Bean // 初始化 Bean
-    @LoadBalanced // 实现负载均衡
+    @Bean // 初始化RestTemplate Bean
+    @LoadBalanced // 实现负载均衡(以rest方式以别名方式进行服务调用，依赖ribbon的负载均衡@LoadBalanced)
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- springcloud-eureka-ribbon/src/main/java/com/example/service/EurekaRibbonService.java
+++ springcloud-eureka-ribbon/src/main/java/com/example/service/EurekaRibbonService.java
@@ -7,15 +7,20 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class EurekaRibbonService {
     @Autowired
-    RestTemplate restTemplate;
- 
+    RestTemplate restTemplate; //springBoot web提供的.底层是httpclient。 默认整合了Ribbon，实现本地的负载均衡
+
     public String getInfo() {
         String message;
         try {
-            System.out.println("调用 服务 EUREKA-CLIENT/info");
+
+            //两种方式调用，一种是固定地址，一种是别名(使用别名到注册中心获取服务调用地址)
+
+            //方式1--别名
             message = restTemplate.getForObject("http://EUREKA-CLIENT/info", String.class);
-            System.out.println("服务 EUREKA-CLIENT/info 返回信息 : " + message);
-            System.out.println("调用 服务 EUREKA-CLIENT/info 成功！");
+            //方式1--固定地址(不正确的方式)
+//            message = restTemplate.getForObject("http://127.0.0.1/info", String.class);
+            //            System.out.println("服务 EUREKA-CLIENT/info 返回信息 : " + message);
+            //            System.out.println("调用 服务 EUREKA-CLIENT/info 成功！");
         } catch (Exception ex) {
             message = ex.getMessage();
         }
